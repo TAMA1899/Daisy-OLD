33666ac15a9a7eda8ed64bbe01c9def79b8bd25b
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-05-22 13:48:55 +0700

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -98,10 +98,8 @@ Saya adalah Group Manager Bot dan saya akan membantu memanage Grub anda.
 ✪ /settings: 
    ◔ in PM: will send you your settings for all supported modules.
    ◔ in a Group: will redirect you to pm, with all that chat's settings.
-""".format(
-    dispatcher.bot.first_name,
-    "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n",
-)
+"""
+
 
 
 DONATE_STRING = """Hey, jika kamu ingin support cukup dengan doa. Terimakasih ❤️
